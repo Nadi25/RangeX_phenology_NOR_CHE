29e8b242caacf52ff41dfda2dab43834e0d2b004
Nadi25 <nadine-arzt@gmx.de>
2026-03-03 22:44:05 +0100

    predict biomass for 23

---
+++ Biomass_prediction_for_23.R
@@ -0,0 +1,22 @@
+
+# source the other script
+
+demo_traits_2023$pred_log_biomass <- predict(m_stems_height2, newdata = demo_traits_2023)
+
+# probably need the same column names 
+# so should maybe rename that in 24 data anyways
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
