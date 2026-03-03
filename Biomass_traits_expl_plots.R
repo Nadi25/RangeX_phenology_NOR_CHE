4097f68364a4d516217e31673027e5241b48b4ec
Nadi25 <nadine-arzt@gmx.de>
2026-03-03 22:08:31 +0100

    plot prediction with raw data

--- Biomass_traits_expl_plots.R
+++ Biomass_traits_expl_plots.R
@@ -1,5 +1,8 @@
 
 
+library(ggeffects)
+
+
 source("Biomass_traits_correlation_NOR.R")
 
 
@@ -51,6 +54,104 @@ ggplot() +
   theme_minimal() +
   theme(legend.position = "right")
 
+
+
+
+
+ggplot(analysis_data, aes(x = no..stems, y = log_biomass)) +
+  geom_point(alpha = 0.6) +
+  geom_smooth(method = "lm", se = TRUE) +
+  theme_minimal() +
+  labs(x = "Number of stems",
+       y = "log(biomass)")
+
+
+ggplot(analysis_data,
+       aes(x = no..stems,
+           y = log_biomass,
+           color = height.rep.stretch..cm.)) +
+  geom_point(alpha = 0.7) +
+  scale_color_viridis_c() +
+  theme_minimal() +
+  labs(x = "Number of stems",
+       y = "log(biomass)",
+       color = "Height (cm)")
+
+
+analysis_data <- analysis_data |>
+  mutate(height_class = cut(height.rep.stretch..cm., 3))
+
+ggplot(analysis_data,
+       aes(x = no..stems, y = log_biomass)) +
+  geom_point(alpha = 0.6) +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~ height_class) +
+  theme_minimal()
+
+
+
+
+# predict data? -----------------------------------------------------------
+# make several classes of height with the quantiles
+# min, low, medium, high, max
+pred <- ggpredict(m_stems_height2,
+                  terms = c("no..stems",
+                            "height.rep.stretch..cm. [quantile]"))
+
+plot(pred)
+
+
+ggplot() +
+  geom_point(data = analysis_data,
+             aes(x = no..stems,
+                 y = log_biomass,
+                 color = height.rep.stretch..cm.),
+             alpha = 0.5) +
+  geom_line(data = pred,
+            aes(x = x,
+                y = predicted,
+                group = group),
+            size = 1.2) +
+  geom_ribbon(data = pred,
+              aes(x = x,
+                  ymin = conf.low,
+                  ymax = conf.high,
+                  group = group),
+              alpha = 0.15) +
+  scale_color_viridis_c() +
+  theme_minimal() +
+  labs(x = "Number of stems",
+       y = "log(biomass)",
+       color = "Height (cm)")
+
+
+
+# plot predicted regression of biomass and no stems and height ------------
+# the lines represent different height classes because the model that fits best 
+# is with height as interaction
+# the dots are the raw data
+ggplot() +
+  geom_point(data = analysis_data,
+             aes(x = no..stems,
+                 y = log_biomass),
+             alpha = 0.3,
+             color = "grey50") +
+  geom_line(data = pred,
+            aes(x = x,
+                y = predicted,
+                color = group),
+            size = 1.3) +
+  geom_ribbon(data = pred,
+              aes(x = x,
+                  ymin = conf.low,
+                  ymax = conf.high,
+                  fill = group),
+              alpha = 0.15) +
+  theme_minimal() +
+  labs(x = "Number of stems",
+       y = "log(biomass total)",
+       color = "Height",
+       fill = "Height")
 
 
 
