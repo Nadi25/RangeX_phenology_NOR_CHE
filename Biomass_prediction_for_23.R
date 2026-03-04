d4dbcf42e81021d6a794546929d9c2d01b405efa
Nadi25 <nadine-arzt@gmx.de>
2026-03-04 20:07:44 +0100

    log no stems

--- Biomass_prediction_for_23.R
+++ Biomass_prediction_for_23.R
@@ -1,5 +1,7 @@
 
-# RangeX biomass - traits preditct 2023 biomass ------------
+# BIOMASS 3 ---------------------------------------------------------------
+
+# RangeX biomass - traits predict 2023 biomass ------------
 
 ## Data used: 
 ## Date:      03.03.26
@@ -23,15 +25,26 @@ demo_traits_2023_bio <- demo_traits_2023 |>
 
 
 
+# log no stems also in 2023 -----------------------------------------------
+# remove 0 stems
+demo_traits_2023_bio <- demo_traits_2023_bio |>
+  filter(no_stems > 0)
+
+demo_traits_2023_bio <- demo_traits_2023_bio |> 
+  mutate(log_no_stems = log(no_stems))
+
+
+
 # use best model to predict biomass for 2023 ------------------------------
 # based on no_stems and height_reproductive_str
 
-demo_traits_2023_bio$pred_log_biomass <- predict(m_stems_height2, newdata = demo_traits_2023_bio)
+demo_traits_2023_bio$pred_log_biomass <- predict(m_stems_height3, 
+                                                 newdata = demo_traits_2023_bio)
 
 
 
 # plotting ----------------------------------------------------------------
-# histogram of prediczed biomass
+# histogram of predicted biomass
 ggplot(demo_traits_2023_bio,
        aes(x = pred_log_biomass)) +
   geom_histogram(bins = 30) +
@@ -40,31 +53,125 @@ ggplot(demo_traits_2023_bio,
 
 # pred biomass vs no stems
 ggplot(demo_traits_2023_bio,
-       aes(x = no_stems,
+       aes(x = log_no_stems,
            y = pred_log_biomass)) +
   geom_point(alpha = 0.6) +
-  labs(x = "Number of stems",
+  labs(x = "Log(number of stems)",
        y = "Predicted log(biomass)")
 
 # plot predicted biomass and observed in same plot
 a <- ggplot() +
   geom_point(data = analysis_data,
-             aes(x = no_stems,
+             aes(x = log_no_stems,
                  y = log_biomass),
              color = "grey50",
              alpha = 0.4) +
   geom_point(data = demo_traits_2023_bio,
-             aes(x = no_stems,
+             aes(x = log_no_stems,
                  y = pred_log_biomass),
              color = "red",
              alpha = 0.6) +
-  labs(x = "Number of stems",
-       y = "log(biomass)",
+  labs(x = "Log(number of stems)",
+       y = "Log(biomass)",
        subtitle = "Grey = observed 2024, Red = predicted 2023")
 a
 
-ggsave(filename = "Output/Biomass_observed_24_prediction_23.png", 
+ggsave(filename = "Output/Biomass/Log_biomass_log_no_stems_observed_24_prediction_23.png", 
        plot = a, width = 13, height = 9, units = "in")
+
+# 
+# analysis_data$species <- droplevels(as.factor(analysis_data$species))
+# demo_traits_2023_bio$species <- droplevels(as.factor(demo_traits_2023_bio$species))
+# 
+# levels(analysis_data$species)
+# levels(demo_traits_2023_bio$species)
+
+# per species
+ggplot() +
+  geom_point(data = analysis_data,
+             aes(x = log_no_stems,
+                 y = log_biomass),
+             color = "grey50",
+             alpha = 0.4) +
+  geom_point(data = demo_traits_2023_bio,
+             aes(x = log_no_stems,
+                 y = pred_log_biomass),
+             color = "red",
+             alpha = 0.6) +
+  facet_grid(~species)+
+  labs(x = "Log(number of stems)",
+       y = "log(biomass)",
+       subtitle = "Grey = observed 2024, Red = predicted 2023")
+
+
+
+
+
+
+# add pred_biomass to demo_23 ---------------------------------------------
+
+# use this dataset with predicted log biomass
+demo_traits_2023_bio
+
+## make correct order as in yearly_demographics
+col_order_traits_23_pb <- c("site", "block_ID", "plot_ID_original","unique_plant_ID", 
+                            "species", "year", "collector", "observer", "height_vegetative_str", 
+                            "height_reproductive_str", "height_vegetative", "height_reproductive", 
+                            "vegetative_width", "vegetative_length", "stem_diameter",
+                            "leaf_length1", "leaf_length2", "leaf_length3", "leaf_width", "petiole_length", 
+                            "petiole_length1", "no_stems",
+                            "number_leaves", "number_tillers", "number_branches", "number_leafclusters", 
+                            "number_flowers", "mean_inflorescence_size", "herbivory", 
+                            "pred_log_biomass", "log_no_stems")
+
+rangex_traits_23_pb <- demo_traits_2023_bio[, col_order_traits_23_pb]
+rangex_traits_23_pb
+
+
+## collector = observer
+## put values from leaf_length in column leaf_length1
+## same with petiole_length
+rangex_traits_23_pb <- rangex_traits_23_pb |> 
+  mutate(collector = coalesce(collector, observer)) |> 
+  mutate(petiole_length = coalesce(petiole_length, petiole_length1)) |> 
+  select(-observer, - petiole_length1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
