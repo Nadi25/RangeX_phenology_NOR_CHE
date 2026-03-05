816fb0aafd1c74c4db0c15afb2523f745ec8e1bb
Nadi25 <nadine-arzt@gmx.de>
2026-03-05 22:16:06 +0100

    model to predict biomass per species

--- Biomass_traits_correlation_per_species.R
+++ Biomass_traits_correlation_per_species.R
@@ -1,4 +1,5 @@
 
+# BIOMASS 3 ---------------------------------------------------------------
 
 # Fit individual models per species ---------------------------------------
 
@@ -287,14 +288,230 @@ AIC(
 # top_models_species[["cennig"]][["additive"]]     5 151.0341
 # top_models_species[["cennig"]][["interaction"]]  6 142.3000  ## best
 
-# final model for sucpra --------------------------------------------------
-# dataset for sucpra
+# final model for cennig --------------------------------------------------
+# dataset for cennig
 df_cennig <- analysis_data_24_log |> filter(species == "cennig")
 
 # final model
-m_cennig <- lmerTest::lmer(log_biomass ~ log_height_reproductive_str * log_no_stems + (1 | block_ID),
-                           data = df_cennig)
+m_cennig <- lmerTest::lmer(log_biomass ~ log_height_reproductive_str * log_no_stems + 
+                             (1 | block_ID), data = df_cennig)
 summary(m_cennig)
+
+
+# pimsax ------------------------------------------------------------------
+# single-trait model for pimsax
+summary(top_models_species[["pimsax"]][["single"]])
+
+# additive model for pimsax
+summary(top_models_species[["pimsax"]][["additive"]])
+
+# interaction model for pimsax
+summary(top_models_species[["pimsax"]][["interaction"]])
+
+# which model is best
+AIC(
+  top_models_species[["pimsax"]][["single"]],
+  top_models_species[["pimsax"]][["additive"]],
+  top_models_species[["pimsax"]][["interaction"]]
+)
+
+# top_models_species[["pimsax"]][["single"]]       4 253.1334
+# top_models_species[["pimsax"]][["additive"]]     5 238.4411  ## best
+# top_models_species[["pimsax"]][["interaction"]]  6 240.7304
+
+# final model for pimsax --------------------------------------------------
+# dataset for pimsax
+df_pimsax <- analysis_data_24_log |> filter(species == "pimsax")
+
+# final model
+m_pimsax <- lmerTest::lmer(log_biomass ~ log_number_leaves + log_no_stems + 
+                             (1 | block_ID), data = df_pimsax)
+summary(m_pimsax)
+
+
+# luzmul ------------------------------------------------------------------
+df_luzmul <- analysis_data_24_log |> filter(species == "luzmul")
+
+# single-trait model
+summary(top_models_species[["luzmul"]][["single"]])
+
+# additive model
+summary(top_models_species[["luzmul"]][["additive"]])
+
+# interaction model
+summary(top_models_species[["luzmul"]][["interaction"]])
+
+# compare models
+AIC(
+  top_models_species[["luzmul"]][["single"]],
+  top_models_species[["luzmul"]][["additive"]],
+  top_models_species[["luzmul"]][["interaction"]]
+)
+
+# top_models_species[["luzmul"]][["single"]]       4 433.8694
+# top_models_species[["luzmul"]][["additive"]]     5 348.5358
+# top_models_species[["luzmul"]][["interaction"]]  6 346.2326  ## best
+
+# final model for luzmul --------------------------------------------------
+m_luzmul <- lmerTest::lmer(
+  log_biomass ~ log_number_leaves * log_no_stems + (1 | block_ID),
+  data = df_luzmul
+)
+summary(m_luzmul)
+
+# leuvul ------------------------------------------------------------------
+df_leuvul <- analysis_data_24_log |> filter(species == "leuvul")
+
+summary(top_models_species[["leuvul"]][["single"]])
+summary(top_models_species[["leuvul"]][["additive"]])
+summary(top_models_species[["leuvul"]][["interaction"]])
+
+AIC(
+  top_models_species[["leuvul"]][["single"]],
+  top_models_species[["leuvul"]][["additive"]],
+  top_models_species[["leuvul"]][["interaction"]]
+)
+
+# top_models_species[["leuvul"]][["single"]]       4 493.5125  ## best
+# top_models_species[["leuvul"]][["additive"]]     5 501.2501
+# top_models_species[["leuvul"]][["interaction"]]  6 506.1067
+
+# final model for leuvul --------------------------------------------------
+m_leuvul <- lmerTest::lmer(
+  log_biomass ~ log_no_stems + (1 | block_ID),
+  data = df_leuvul
+)
+summary(m_leuvul)
+
+# tripra ------------------------------------------------------------------
+df_tripra <- analysis_data_24_log |> filter(species == "tripra")
+
+summary(top_models_species[["tripra"]][["single"]])
+summary(top_models_species[["tripra"]][["additive"]])
+summary(top_models_species[["tripra"]][["interaction"]])
+
+AIC(
+  top_models_species[["tripra"]][["single"]],
+  top_models_species[["tripra"]][["additive"]],
+  top_models_species[["tripra"]][["interaction"]]
+)
+
+# top_models_species[["tripra"]][["single"]]       4 381.9630  ## best
+# top_models_species[["tripra"]][["additive"]]     5 387.1187
+# top_models_species[["tripra"]][["interaction"]]  6 391.1435
+
+# final model for tripra --------------------------------------------------
+m_tripra <- lmerTest::lmer(
+  log_biomass ~ log_height_reproductive_str  + (1 | block_ID),
+  data = df_tripra
+)
+summary(m_tripra)
+
+
+# hypmac ------------------------------------------------------------------
+df_hypmac <- analysis_data_24_log |> filter(species == "hypmac")
+
+summary(top_models_species[["hypmac"]][["single"]])
+summary(top_models_species[["hypmac"]][["additive"]])
+summary(top_models_species[["hypmac"]][["interaction"]])
+
+AIC(
+  top_models_species[["hypmac"]][["single"]],
+  top_models_species[["hypmac"]][["additive"]],
+  top_models_species[["hypmac"]][["interaction"]]
+)
+
+# top_models_species[["hypmac"]][["single"]]       4 261.8300
+# top_models_species[["hypmac"]][["additive"]]     5 261.6323  ## best
+# top_models_species[["hypmac"]][["interaction"]]  6 263.7201
+
+# final model for hypmac --------------------------------------------------
+m_hypmac <- lmerTest::lmer(
+  log_biomass ~ log_number_leaves + log_no_stems + (1 | block_ID),
+  data = df_hypmac
+)
+summary(m_hypmac)
+
+# plalan ------------------------------------------------------------------
+df_plalan <- analysis_data_24_log |> filter(species == "plalan")
+
+summary(top_models_species[["plalan"]][["single"]])
+summary(top_models_species[["plalan"]][["additive"]])
+summary(top_models_species[["plalan"]][["interaction"]])
+
+AIC(
+  top_models_species[["plalan"]][["single"]],
+  top_models_species[["plalan"]][["additive"]],
+  top_models_species[["plalan"]][["interaction"]]
+)
+
+# top_models_species[["plalan"]][["single"]]       4 403.5728
+# top_models_species[["plalan"]][["additive"]]     5 402.1336  ## best
+# top_models_species[["plalan"]][["interaction"]]  6 403.0608
+
+# final model for plalan --------------------------------------------------
+m_plalan <- lmerTest::lmer(
+  log_biomass ~ log_height_vegetative_str + log_height_reproductive_str + (1 | block_ID),
+  data = df_plalan
+)
+summary(m_plalan)
+
+# cyncri ------------------------------------------------------------------
+df_cyncri <- analysis_data_24_log |> filter(species == "cyncri")
+
+summary(top_models_species[["cyncri"]][["single"]])
+summary(top_models_species[["cyncri"]][["additive"]])
+summary(top_models_species[["cyncri"]][["interaction"]])
+
+AIC(
+  top_models_species[["cyncri"]][["single"]],
+  top_models_species[["cyncri"]][["additive"]],
+  top_models_species[["cyncri"]][["interaction"]]
+)
+
+# top_models_species[["cyncri"]][["single"]]       4 213.8145  ## best
+# top_models_species[["cyncri"]][["additive"]]     5 216.3082
+# top_models_species[["cyncri"]][["interaction"]]  6 214.3393
+
+# final model for cyncri --------------------------------------------------
+m_cyncri <- lmerTest::lmer(
+  log_biomass ~ log_no_stems + (1 | block_ID),
+  data = df_cyncri
+)
+summary(m_cyncri)
+
+# sildio ------------------------------------------------------------------
+df_sildio <- analysis_data_24_log |> filter(species == "sildio")
+
+summary(top_models_species[["sildio"]][["single"]])
+summary(top_models_species[["sildio"]][["additive"]])
+summary(top_models_species[["sildio"]][["interaction"]])
+
+AIC(
+  top_models_species[["sildio"]][["single"]],
+  top_models_species[["sildio"]][["additive"]],
+  top_models_species[["sildio"]][["interaction"]]
+)
+
+# top_models_species[["sildio"]][["single"]]       4 17.92125
+# top_models_species[["sildio"]][["additive"]]     4 17.92125
+# top_models_species[["sildio"]][["interaction"]]  4 17.92125
+
+# final model for sildio --------------------------------------------------
+m_sildio <- lmerTest::lmer(
+  log_biomass ~ log_number_leaves + (1 | block_ID),
+  data = df_sildio
+)
+summary(m_sildio)
+
+
+
+
+
+
+
+
+
 
 
 
